bae33c445e430097399c271f0200ecfffa8a2751
StarodubcevDV <47948733+StarodubcevDV@users.noreply.github.com>
2024-03-17 18:19:20 +0500

    Feature/add environment setup (#2)
    * feat(environment): add base environment for tests
    
    * ci(tests): add build and push image
    
    * ci(tests): fix actions yml file
    
    * ci(tests): add run unit tests job
    
    * fix(library setup): fix library setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='c2py',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python library which provide C2PA standard functionality.',
+    author='StarodubcevDV',
+    license='MIT',
+)
