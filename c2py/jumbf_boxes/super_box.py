1b499f04a6ad47ec8918fcb3b23bd3d39b72ae29
StarodubcevDV <starodubtcev.d.v@gmail.com>
2024-03-31 00:44:55 +0500

    chore(jumbf): add serialization for super box

--- c2py/jumbf_boxes/super_box.py
+++ c2py/jumbf_boxes/super_box.py
@@ -10,6 +10,29 @@ class SuperBox(Box):
     description_box = None
     content_boxes = []
 
-    def __init__(self, content_type = jumbf_content_types["json"], label=''):
-        super().__init__('jumb'.encode('utf-8').hex())
+    def __init__(self, content_boxes = [], content_type = jumbf_content_types["json"], label=''):
         self.description_box = DescriptionBox(content_type=content_type, label=label)
+        self.content_boxes = content_boxes
+
+        self.payload = self.description_box.serialize() + self.serialize_content_boxes()
+        super().__init__('jumb'.encode('utf-8').hex())
+
+    
+    def add_content_box(self, content_box):
+        self.content_boxes.append(content_box)
+
+
+    def serialize_content_boxes(self):
+
+        serialized_content_boxes = b''
+
+        for content_box in self.content_boxes:
+            serialized_content_boxes += content_box.serialize()
+
+        return serialized_content_boxes
+    
+
+    def sync_payload(self):
+        self.payload = self.description_box.serialize() + self.serialize_content_boxes()
+        super().__init__('jumb'.encode('utf-8').hex())
+
--- tests/jumbf/super_boxes_test.py
+++ tests/jumbf/super_boxes_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from c2py.jumbf_boxes.super_box import SuperBox
+from c2py.jumbf_boxes.content_box import ContentBox
 from c2py.jumbf_boxes.jumbf_content_types import jumbf_content_types
 
 
@@ -37,4 +38,46 @@ def test_create_super_box_with_content_boxes():
     test_super_box = SuperBox()
 
     assert len(test_super_box.content_boxes) == 0 
+
+
+def test_serialize_super_box():
+
+    test_super_box = SuperBox(label='super')
+
+    test_serialized_data = b'\x6a\x75\x6d\x62' + \
+                            b'\x00\x00\x00\x27' + \
+                            b'\x6a\x75\x6d\x64' + \
+                            b'\x00\x00\x00\x1f' + \
+                            b'\x6a\x73\x6f\x6e\x00\x11\x00\x10\x80\x00\x00\xaa\x00\x38\x9b\x71' + \
+                            b'\x03' + \
+                            b'\x73\x75\x70\x65\x72' + \
+                            b'\x00'
+    
+    print(test_super_box.get_length())
+
+    assert test_super_box.serialize() == test_serialized_data
+
+
+def test_serialize_super_box_with_content_box():
+
+    test_super_box = SuperBox(label='super')
+
+    test_content_box = ContentBox(payload=b'\x00\x00')
+    test_super_box.add_content_box(test_content_box)
+
+    test_super_box.sync_payload()
+
+    test_serialized_data = b'\x6a\x75\x6d\x62' + \
+                            b'\x00\x00\x00\x31' + \
+                            b'\x6a\x75\x6d\x64' + \
+                            b'\x00\x00\x00\x1f' + \
+                            b'\x6a\x73\x6f\x6e\x00\x11\x00\x10\x80\x00\x00\xaa\x00\x38\x9b\x71' + \
+                            b'\x03' + \
+                            b'\x73\x75\x70\x65\x72' + \
+                            b'\x00' + \
+                            b'\x6a\x75\x6d\x62' + \
+                            b'\x00\x00\x00\x0A' + \
+                            b'\x00\x00'
+
+    assert test_super_box.serialize() == test_serialized_data
 
