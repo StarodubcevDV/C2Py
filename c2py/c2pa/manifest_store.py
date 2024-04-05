f1d308151ff23d55b11d3218946c5ac9bf8ead63
StarodubcevDV <starodubtcev.d.v@gmail.com>
2024-04-05 17:54:36 +0500

    fix(c2pa): add check for adding manifests to manifest store

--- c2py/c2pa/manifest_store.py
+++ c2py/c2pa/manifest_store.py
@@ -5,7 +5,7 @@ from c2py.utils.content_types import c2pa_content_types
 
 class ManifestStore(SuperBox):
 
-    def __init__(self, manifests=[]):
-        self.manifests = manifests
+    def __init__(self, manifests=None):
+        self.manifests = [] if manifests == None else manifests
         super().__init__(content_type=c2pa_content_types['manifest_store'], label='c2pa', content_boxes=manifests)
 
