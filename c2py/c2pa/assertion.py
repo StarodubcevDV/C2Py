0a748d2b309ec38aad874d49c20dccb8034737bf
StarodubcevDV <47948733+StarodubcevDV@users.noreply.github.com>
2024-04-15 23:07:21 +0500

    chore(c2pa): add c2pa assertions (#6)
    * chore(c2pa): add base assertion structure
    
    * chore(c2pa): add payload serialization for assertions

---
+++ c2py/c2pa/assertion.py
@@ -0,0 +1,28 @@
+
+from c2py.jumbf_boxes.super_box import SuperBox
+from c2py.jumbf_boxes.content_box import ContentBox
+from c2py.utils.assertion_schemas import get_assertion_content_type, get_assertion_label, json_to_bytes, cbor_to_bytes
+from c2py.utils.content_types import jumbf_content_types
+
+
+class Assertion(SuperBox):
+
+    def __init__(self, assertion_type, schema):
+        self.type = assertion_type
+        self.schema = schema
+        content_box = ContentBox(payload=self.get_payload_from_schema())
+
+        super().__init__(content_type=get_assertion_content_type(assertion_type),
+                         label=get_assertion_label(assertion_type),
+                         content_boxes=[content_box])
+
+
+    def get_payload_from_schema(self):
+        if get_assertion_content_type(self.type) == jumbf_content_types['json']:
+            return json_to_bytes(self.schema)
+        elif get_assertion_content_type(self.type) == jumbf_content_types['cbor']:
+            return cbor_to_bytes(self.schema)
+        elif get_assertion_content_type(self.type) == jumbf_content_types['codestream']:
+            return self.schema['payload']
+        else:
+            return b''
