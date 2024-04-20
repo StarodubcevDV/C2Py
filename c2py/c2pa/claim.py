2332b1ddb0bcf89361279c823f982cff897f3c41
StarodubcevDV <47948733+StarodubcevDV@users.noreply.github.com>
2024-04-20 10:16:57 +0500

    chore(c2pa): add c2pa claim (#8)
    * chore(c2pa): add assertion store (#7)
    
    * chore(c2pa): add base claim structure
    
    * chore(c2pa): add hashing of assertions

---
+++ c2py/c2pa/claim.py
@@ -0,0 +1,39 @@
+
+import hashlib
+
+from c2py.jumbf_boxes.super_box import SuperBox
+from c2py.jumbf_boxes.content_box import ContentBox
+from c2py.utils.content_types import c2pa_content_types, jumbf_content_types
+from c2py.utils.assertion_schemas import get_assertion_label, cbor_to_bytes
+
+
+class Claim(SuperBox):
+
+    def __init__(self, assertion_store, claim_generator='', manifest_label=''):
+        self.claim_generator = claim_generator
+        self.manifest_label = manifest_label
+        self.claim_signature_label = f'self#jumbf=c2pa/{self.manifest_label}/c2pa.signature'
+
+        content_boxes = []
+        self.assertion_store = assertion_store
+        if self.assertion_store != None:
+            content_box = ContentBox(box_type='cbor'.encode('utf-8').hex(), payload=self.generate_claim_schema())
+            content_boxes.append(content_box)
+
+        super().__init__(label='c2pa.claim', content_type=c2pa_content_types["claim"], content_boxes=content_boxes)
+
+    
+    def generate_claim_schema(self):
+        claim_schema = {}
+        claim_schema['alg'] = "sha256"
+        claim_schema['claim_generator'] = self.claim_generator
+        claim_schema['signature'] = f'self#jumbf=c2pa/{self.manifest_label}/c2pa.signature"'
+        claim_schema['assertions'] = [
+            {
+                'url': f'self#jumbf=c2pa/{self.manifest_label}/c2pa.assertions/{get_assertion_label(assertion.type)}',
+                'hash': (hashlib.sha256(assertion.serialize())).digest()
+            } for assertion in self.assertion_store.assertions
+        ]
+
+        return cbor_to_bytes(claim_schema)
+
--- c2py/jumbf_boxes/content_box.py
+++ c2py/jumbf_boxes/content_box.py
@@ -6,6 +6,6 @@ from c2py.utils.content_types import jumbf_content_types
 
 class ContentBox(Box):
 
-    def __init__(self, payload = b''):
-        super().__init__('jumb'.encode('utf-8').hex(), payload=payload)
+    def __init__(self, box_type='json'.encode('utf-8').hex(), payload = b''):
+        super().__init__(box_type=box_type, payload=payload)
     
