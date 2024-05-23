c270aa222d01c7dacc6d55f225443e7484c80a1f
StarodubcevDV <starodubtcev.d.v@gmail.com>
2024-05-23 22:46:08 +0500

    fix: fix test app

--- c2py_test_app/test_app.py
+++ c2py_test_app/test_app.py
@@ -3,8 +3,8 @@ import hashlib
 from c2py.interface import C2Py_GenerateAssertion, C2Py_GenerateManifest, C2Py_EmplaceManifest, C2PA_AssertionTypes, C2PA_ContentTypes
 
 
-key_filepath = 'tests/fixtures/crypto/ps256.pem'
-cert_filepath = 'tests/fixtures/crypto/ps256.pub'
+key_filepath = '../tests/fixtures/crypto/ps256.pem'
+cert_filepath = '../tests/fixtures/crypto/ps256.pub'
 
 if key_filepath != '':
     with open(key_filepath, 'rb') as f:
@@ -18,42 +18,39 @@ if cert_filepath != '':
 else:
     key = []
 
+cai_offset = 2
 
-def __main__():
-    
-    cai_offset = 2
-    
-    with open("tests/fixtures/test_image.jpg", "rb") as binary_image:
-        raw_bytes = binary_image.read()
+with open("../tests/fixtures/test_image.jpg", "rb") as binary_image:
+    raw_bytes = binary_image.read()
 
-    creative_work_schema = {
-        "@context": "https://schema.org",
-        "@type": "CreativeWork",
-        "author": [
-            {
-                "@type": "Person",
-                "name": "Dmitriy Starodubtcev"
-            }
-        ],
-        "copyrightYear": "2024",
-        "copyrightHolder": "c2py"
-    }
-    creative_work_assertion = C2Py_GenerateAssertion(C2PA_AssertionTypes.creative_work, creative_work_schema)
-    
-    hash_data_schema = {
-        "exclusions": [{"start": cai_offset, "length": 65535}], # set length to 65535, library will calculate length by itself
-        "name": "jumbf manifest",
-        "alg": "sha256",
-        "hash": hashlib.sha256(raw_bytes).digest(),
-        "pad": []
-    }
-    hash_data_assertion = C2Py_GenerateAssertion(C2PA_AssertionTypes.data_hash, hash_data_schema)
-    
-    assertions = [creative_work_assertion, hash_data_assertion]
-    
-    manifest = C2Py_GenerateManifest(assertions=assertions, private_key=key, certificate_chain=certificate)
-    
-    raw_bytes = C2Py_EmplaceManifest(C2PA_ContentTypes.jpg, raw_bytes, cai_offset, manifest)
-    
-    with open("test_injected_image.jpg", "wb") as binary_file:
-        binary_file.write(raw_bytes)
+creative_work_schema = {
+    "@context": "https://schema.org",
+    "@type": "CreativeWork",
+    "author": [
+        {
+            "@type": "Person",
+            "name": "Dmitriy Starodubtcev"
+        }
+    ],
+    "copyrightYear": "2024",
+    "copyrightHolder": "c2py"
+}
+creative_work_assertion = C2Py_GenerateAssertion(C2PA_AssertionTypes.creative_work, creative_work_schema)
+
+hash_data_schema = {
+    "exclusions": [{"start": cai_offset, "length": 65535}], # set length to 65535, library will calculate length by itself
+    "name": "jumbf manifest",
+    "alg": "sha256",
+    "hash": hashlib.sha256(raw_bytes).digest(),
+    "pad": []
+}
+hash_data_assertion = C2Py_GenerateAssertion(C2PA_AssertionTypes.data_hash, hash_data_schema)
+
+assertions = [creative_work_assertion, hash_data_assertion]
+
+manifest = C2Py_GenerateManifest(assertions=assertions, private_key=key, certificate_chain=certificate)
+
+raw_bytes = C2Py_EmplaceManifest(C2PA_ContentTypes.jpg, raw_bytes, cai_offset, manifest)
+
+with open("test_injected_image.jpg", "wb") as binary_file:
+    binary_file.write(raw_bytes)
